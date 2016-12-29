4d3d6bb41702e88d320214cdc28860ff2dad41b2
Zer0 Mem0ry <zeromemor@gmail.com>
2016-12-29 20:03:51 +0200

    Update main.cpp

--- CrackCRC/main.cpp
+++ CrackCRC/main.cpp
@@ -1,50 +1,49 @@
 #include <iostream>
 #include <string>
 
-#include <Windows.h>
-
 #include "CRC.h"
 
 using namespace std;
 
-// 
 string numerals = "0123456789";
 string alphabetLower = "abcdefghijklmnopqrstuvwxyz";
 string alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string specialAnsi = "!\"#¤%&/()=?`^¨*';:_,.";
+string specialAnsi = "!\"#Â¤%&/()=?`^Â¨*';:_,.";
 
-
+// our brute force function
 void CrackHash(string source, unsigned int hash, string seed, size_t length)
 {
-	// this is used to measure time
-	static const int startTick = GetTickCount64(); 
+	static const int startTick = GetTickCount64();
 
-	static int iterations; // store the count of iterations
-	static int collisions; // store the count of collisions/hash
+	static int iterations; // hold the count of iterations
+	static int collisions; // hold the count of collisions / hash
 
-	iterations++; // increment iteration count each time function is executed.
+	iterations++; // increment on each execution
 
-	if (length == 0) // if the length is reached
+	if (length == 0)
 	{
-		if (CRC32(seed.c_str(), seed.length()) == hash) // if the hash created from seed matches the input
+		// if input hash matches with seed hash
+		if (CRC32(seed.c_str(), seed.length()) == hash)
 		{
-			// calculate the time the operation took
+			// calculate time used
 			double endTick = GetTickCount64() - startTick;
-			collisions++; // increment collisions
+			collisions++;
 
+			// output
 			cout << "Collision #" << collisions << ", Source: " << seed << ", Hash: 0x" << StrCRC32(seed)
-				 << ", Iterations: " << iterations << endl << "Ticks: " << endTick << ", Seconds: "
-				 << endTick / 1000 << ", Minutes: " << endTick / 1000 / 60 << ", Hours: " << endTick / 1000 / 60 / 60;
+				<< ", Iterations: " << iterations << endl << "Ticks: " << endTick << ", Seconds: "
+				<< endTick / 1000 << ", Minutes: " << endTick / 1000 / 60 << ", Hours: " << endTick / 1000 / 60 / 60;
 			cout << endl << endl;
 		}
 		return;
 	}
 
-	// loop through the provided characters
+	// iterate through the input stringspace
 	for (size_t i = 0; i < source.length(); i++)
 	{
 		string appended = seed + source[i];
 
+		// continue the loop
 		CrackHash(source, hash, appended, length - 1);
 	}
 }
@@ -52,17 +51,16 @@ void CrackHash(string source, unsigned int hash, string seed, size_t length)
 
 int main()
 {
-	// our target hash is 'test' hashed
+	// our targethash, which we are going to crack
 	unsigned int targetHash = 0xd87f7e0c;
-	//cout << "CRC32 hash of 'test' is: " << StrCRC32("test") << endl;
-	//cin.get();
 
-	// now let's try to crack the hash of test
+	// loop our crackhash function
 	while (1)
 	{
-		// the length of string that the algorithm starts from
+		// string startlength
 		static unsigned int strlen = 1;
-		CrackHash(alphabetLower + alphabetUpper + numerals + specialAnsi, targetHash, "", strlen);
-		strlen++;
+		CrackHash(alphabetLower + alphabetUpper + numerals + specialAnsi,
+			targetHash, "", strlen);
+		strlen++; // increment the seed length after each iteration
 	}
 }
